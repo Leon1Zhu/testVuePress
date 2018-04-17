31efb111d4b77542cd93612b7bd10b56808400f0
zhuL <1161107512@qq.com.com>
2018-04-17 23:10:00 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   description: '文件上传组件 vue文件上传 多文件上传',
   port:8888,
   themeConfig: {
-    repo: 'ZhuLiangT/testVuePress',
+    repo: 'ZhuLiangT/v-alien-upload',
     sidebar: [
       ['/', '起步'],
       ['/example', '示例'],
